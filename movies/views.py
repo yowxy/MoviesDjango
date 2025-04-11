e41ea124d347f11fe1edd0f57773bda74e0e18dc
Iklil <iklilblender@gmail.com>
2025-04-12 03:25:23 +0700

    feat: view templates

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,40 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+
+data = {
+    'movies': [
+        {
+            'id': 1,
+            'title': 'The Shawshank Redemption',
+            'year': 1994
+        },
+        {
+            'id': 2,
+            'title': 'The Godfather',
+            'year': 1972
+        },
+        {
+            'id': 3,
+            'title': 'The Dark Knight',
+            'year': 2008
+        },
+        {
+            'id': 4,
+            'title': '12 Angry Men',
+            'year': 1957
+        },
+        {
+            'id': 5,
+            'title': "Schindler's List",
+            'year': 1993
+        }
+    ]
+}
 
 def movies (request):
-    return  HttpResponse('Hello World')
+    return  render(request,'movies/movies.html', data)
 
 
 def home(request):
-    return HttpResponse('Home Page')
+    return HttpResponse('Home  Page')
